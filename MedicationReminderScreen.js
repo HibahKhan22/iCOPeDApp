10f549475306b7e7edf1054c786586685b97c2fe
HibahKhan22 <78056863+HibahKhan22@users.noreply.github.com>
2021-04-02 06:26:04 -0400

    Create MedicationReminderScreen.js

---
+++ MedicationReminderScreen.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  StatusBar,
+  TouchableOpacity,
+  ScrollView,
+  Linking,
+} from 'react-native';
+import {useTheme} from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Icon from 'react-native-vector-icons/Ionicons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import {createAppContainer} from 'react-navigation';
+import HomeButton from '../components/HomeButton';
+
+const HomeScreen = ({navigation}) => {
+  const theme = useTheme();
+
+  return (
+  
+  <View style={styles.cardsWrapper}>
+
+        <View style={styles.card}>
+        
+          <View style={styles.cardInfo}>
+            <Text style={styles.cardTitle}>Medical</Text>
+            
+           </View>
+           
+             <View style={styles.cardsWrapper}>
+
+        <View style={styles.card}>
+        
+          <View style={styles.cardInfo}>
+            <Text style={styles.cardTitle}>Upcoming</Text>
+            
+           </View>
+           
+            <View style={styles.cardsWrapper}>
+
+        <View style={styles.card}>
+        
+          <View style={styles.cardInfo}>
+            
+            <Text style={styles.cardDetails}>
+            Inhale 20 mcg of Budesonide via Pulmicort Flexhaler
+            </Text>
+
+          </View>
+           
